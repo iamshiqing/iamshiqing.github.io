64395d348071f120ecc6ec67de84dce5bc545cb9
iamshiqing <183805476@qq.com>
2024-04-06 13:40:25 +0800

    new

--- assets/js/events.js
+++ assets/js/events.js
@@ -35,34 +35,34 @@ Fluid.events = {
     });
   },
 
-  // registerParallaxEvent: function() {
-  //   var ph = jQuery('#banner[parallax="true"]');
-  //   if (ph.length === 0) {
-  //     return;
-  //   }
-  //   var board = jQuery('#board');
-  //   if (board.length === 0) {
-  //     return;
-  //   }
-  //   var parallax = function() {
-  //     var pxv = jQuery(window).scrollTop() / 5;
-  //     var offset = parseInt(board.css('margin-top'), 10);
-  //     var max = 96 + offset;
-  //     if (pxv > max) {
-  //       pxv = max;
-  //     }
-  //     ph.css({
-  //       transform: 'translate3d(0,' + pxv + 'px,0)'
-  //     });
-  //     var sideCol = jQuery('.side-col');
-  //     if (sideCol) {
-  //       sideCol.css({
-  //         'padding-top': pxv + 'px'
-  //       });
-  //     }
-  //   };
-  //   Fluid.utils.listenScroll(parallax);
-  // },
+  registerParallaxEvent: function() {
+    var ph = jQuery('#banner[parallax="true"]');
+    if (ph.length === 0) {
+      return;
+    }
+    var board = jQuery('#board');
+    if (board.length === 0) {
+      return;
+    }
+    var parallax = function() {
+      var pxv = jQuery(window).scrollTop() / 5;
+      var offset = parseInt(board.css('margin-top'), 10);
+      var max = 96 + offset;
+      if (pxv > max) {
+        pxv = max;
+      }
+      ph.css({
+        transform: 'translate3d(0,' + pxv + 'px,0)'
+      });
+      var sideCol = jQuery('.side-col');
+      if (sideCol) {
+        sideCol.css({
+          'padding-top': pxv + 'px'
+        });
+      }
+    };
+    Fluid.utils.listenScroll(parallax);
+  },
 
   registerScrollDownArrowEvent: function() {
     var scrollbar = jQuery('.scroll-down-bar');
